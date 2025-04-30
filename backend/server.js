92708cee9c03920186f063bfb413df29e7710b85
dharmeshgidwani <rajeshsonia50.rg@gmail.com>
2025-04-30 21:56:57 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,24 @@ const orderRoutes = require("./routes/orderRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const userRoutes = require("./routes/userRoutes");
 
+const allowedOrigins = [
+  "http://localhost:5174",
+  "https://book-lovers.vercel.app",
+];
+
+
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
