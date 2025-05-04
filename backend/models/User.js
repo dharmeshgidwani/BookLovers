51b585bdec8ae88427f7ef2f63eda149931b92d5
dharmeshgidwani <rajeshsonia50.rg@gmail.com>
2025-05-04 17:17:52 +0530

    address issue

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true},
   phone: {type: Number,unique: true, required: true},
   password: { type: String, required: true },
-  address: { type: String, required: true },
+  address: { type: String },
   role: { type: String, enum: ["user", "admin"], default: "user" },
   cart: [
     {
