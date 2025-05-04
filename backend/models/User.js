0a50e77f39ea21a10658b6462b88549e2700b88e
dharmeshgidwani <rajeshsonia50.rg@gmail.com>
2025-05-04 13:49:56 +0530

    email optional

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
+  email: { type: String, unique: true},
   phone: {type: Number,unique: true, required: true},
   password: { type: String, required: true },
   address: { type: String, required: true },
